789183e35132f9eaf7435672038e441298ff758f
DerekYuYi <1363364123@qq.com>
2019-08-02 14:05:49 +0800

    Updates README.md.

--- SwiftMDAdDemo/Pods/Target Support Files/Pods-SwiftMDAdDemo/Pods-SwiftMDAdDemo-frameworks.sh
+++ SwiftMDAdDemo/Pods/Target Support Files/Pods-SwiftMDAdDemo/Pods-SwiftMDAdDemo-frameworks.sh
@@ -161,10 +161,10 @@ strip_invalid_archs() {
 
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
-  install_framework "${PODS_ROOT}/RuiYuAdMob/RuiYuAdMob.framework"
+  install_framework "${PODS_ROOT}/MDAd/MDAd.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
-  install_framework "${PODS_ROOT}/RuiYuAdMob/RuiYuAdMob.framework"
+  install_framework "${PODS_ROOT}/MDAd/MDAd.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
--- SwiftMDAdDemo/Pods/Target Support Files/Pods-SwiftMDAdDemo/Pods-SwiftMDAdDemo-resources.sh
+++ SwiftMDAdDemo/Pods/Target Support Files/Pods-SwiftMDAdDemo/Pods-SwiftMDAdDemo-resources.sh
@@ -97,10 +97,10 @@ EOM
   esac
 }
 if [[ "$CONFIGURATION" == "Debug" ]]; then
-  install_resource "${PODS_ROOT}/RuiYuAdMob/RuiYuAdMob.bundle"
+  install_resource "${PODS_ROOT}/MDAd/MDAd.bundle"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
-  install_resource "${PODS_ROOT}/RuiYuAdMob/RuiYuAdMob.bundle"
+  install_resource "${PODS_ROOT}/MDAd/MDAd.bundle"
 fi
 
 mkdir -p "${TARGET_BUILD_DIR}/${UNLOCALIZED_RESOURCES_FOLDER_PATH}"
