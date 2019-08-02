b5a080ec44ae5f883d1da3d32b095cc246f29be3
DerekYuYi <1363364123@qq.com>
2019-08-02 15:47:04 +0800

    update OCMDAdDemo with pods.

---
+++ OCMDAdDemo/Pods/MDAd/MDAd.framework/Headers/MDAd.h
@@ -0,0 +1,19 @@
+//
+//  MDAd.h
+//  MDAd
+//
+//  Created by DerekYuYi on 2019/6/6.
+//  Copyright © 2019 RuiYu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MDAd.
+FOUNDATION_EXPORT double MDAdVersionNumber;
+
+//! Project version string for MDAd.
+FOUNDATION_EXPORT const unsigned char MDAdVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MDAd/PublicHeader.h>
+
+
---
+++ OCMDAdDemo/Pods/Target Support Files/Pods-OCMDAdDemo/Pods-OCMDAdDemo-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_OCMDAdDemo : NSObject
+@end
+@implementation PodsDummy_Pods_OCMDAdDemo
+@end
